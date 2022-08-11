60f3d8bc846ef2208816211bd8d669e7e8e11007
Chris Pickett <chris.pickett@prefect.io>
2022-08-11 15:14:13 -0700

    Implicitly mark default kewyord arguments to the task function as being unmapped.

--- tests/utilities/test_callables.py
+++ tests/utilities/test_callables.py
@@ -9,6 +9,20 @@ from packaging.version import Version
 from prefect.utilities import callables
 
 
+class TestGetCallDefaults:
+    def test_function_without_defaults(self):
+        def f(a, b, c):
+            pass
+
+        assert callables.get_call_defaults(f) == {}
+
+    def test_function_with_defaults(self):
+        def f(a, b=1, c="hello"):
+            pass
+
+        assert callables.get_call_defaults(f) == {"b": 1, "c": "hello"}
+
+
 class TestFunctionToSchema:
     def test_simple_function_with_no_arguments(self):
         def f():
